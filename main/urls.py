7dc0c256a5aeabb5d0e31087ea71cfe816bdcd46
Salsabila Aulia <salsabilaaulia@Salsabilas-MacBook-Pro.local>
2023-10-04 15:44:59 +0700

    menambahkan modals

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import show_main
 from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml 
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, get_product_json, add_product_ajax
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
@@ -23,4 +23,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
 ]
