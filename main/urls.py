374c7af43bf62b1a5d5de63b38c0a18faeb6b7ce
Salsabila Aulia <salsabilaaulia@Salsabilas-MacBook-Pro.local>
2023-11-15 13:48:25 +0700

    menambahkan app authentication

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import show_main
 from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml 
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, get_product_json, add_product_ajax
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, get_product_json, add_product_ajax, create_product_flutter
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
@@ -24,5 +24,6 @@ urlpatterns = [
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('get-product/', get_product_json, name='get_product_json'),
-    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -15,6 +15,8 @@ import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
 
 def create_product(request):
     form = ProductForm(request.POST or None)
@@ -130,3 +132,22 @@ def add_product_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
