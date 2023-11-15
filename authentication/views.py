b5e5b1f613e6a4618e573ee867b76e442928cc0e
Salsabila Aulia <salsabilaaulia@Salsabilas-MacBook-Pro.local>
2023-11-15 14:35:52 +0700

    minor fix logout

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
